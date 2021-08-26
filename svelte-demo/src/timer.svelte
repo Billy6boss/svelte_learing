da8cbb6e81e0e08c6a3807c9c1a29a3774bff97c
Billy <38741030+Billy6boss@users.noreply.github.com>
2021-08-26 16:28:46 +0800

    Add time in WordsAndInput export of value and function Option to hide buttons of timer
    Add input binding and timer bind
    Start count after input and stop after 1 min
    word correct an worng count
    styling

--- svelte-demo/src/timer.svelte
+++ svelte-demo/src/timer.svelte
@@ -1,7 +1,12 @@
 <script>
-    let mic_sec = 0;
-    let min = 0;
+import { each } from "svelte/internal";
+
+
+    export let mic_sec = 0;
+    export let min = 0;
     let hour = 0;
+    export let count = 0;
+    export let hideButton = false;
 
     // $:  mean depent on ...
 
@@ -14,35 +19,47 @@
 
     let timer;
 
-    function startTimer() {
-        timer = window.setInterval(function () {
-            mic_sec += 1;
-            if (mic_sec / 100 >= 60) {
-                min++;
-                mic_sec = 0;
-            }
-        }, 10);
+    export function startTimer() {
+        
+        if(!timer){
+            timer = window.setInterval(function () {
+                mic_sec += 1;
+                if (mic_sec / 100 >= 60) {
+                    min++;
+                    mic_sec = 0;
+                }
+            }, 10);
+        }
+
     }
 
-    function stopTimer() {
-        window.clearTimeout(timer);
+    export function stopTimer() {
+        window.clearInterval(timer);
+        timer = null;
     }
 
-    function resetTimer() {
+    export function resetTimer() {
+        count++;
         mic_sec = 0;
         min = 0;
         hour = 0;
     }
 </script>
 
-<button class="btn btn-success" on:click={startTimer}>開始</button>
-<button class="btn btn-danger" on:click={stopTimer}>Stop</button>
-<button class="btn btn-secondary" on:click={resetTimer}>Reset</button>
+{#if !hideButton}
+    <button class="btn btn-success" on:click={startTimer}>開始</button>
+    <button class="btn btn-danger" on:click={stopTimer}>Stop</button>
+    <button class="btn btn-secondary" on:click={resetTimer}>Reset</button>
+{/if}
 <h5>
     Time in: {hour}{blink}{min}{blink}<span class="sec-box"
         >{(mic_sec / 100).toFixed(2)}</span
     >
 </h5>
+<label for="">
+    <input type="checkbox" bind:checked ={hideButton}>
+    Hide the button
+</label>
 
 <style>
     .sec-box {
